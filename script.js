2d59d2e5227d5bc7b7c60ef3c9dbecb43b6eaf55
timre13 <itorteli13@gmail.com>
2022-10-29 22:32:35 +0200

    Underline current section header item

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 let imgs = document.getElementsByClassName("animated-img");
+let sections = document.querySelectorAll("section")
 
 const obs = new IntersectionObserver((entries, obs) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add("animated");
-            console.log(entry);
+            //console.log(entry);
         }
     });
 });
+
+
+const ratioLimit = 0.522
+const sectObs = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+            //document.querySelector(`[href*='#${entry.target.id}']`).classList.remove("active")
+            console.log(entry.target.id, entry.intersectionRatio)
+        if (entry.isIntersecting && entry.intersectionRatio >= ratioLimit) {
+            document.querySelector(`[href*='#${entry.target.id}']`)?.classList.add("active")
+        } else {
+            document.querySelector(`[href*='#${entry.target.id}']`)?.classList.remove("active")
+            //document.querySelectorAll("#header>a")
+        }
+    })
+}, {threshold: [0.25, ratioLimit, 0.75]})
 
 for (let img of imgs) {
     obs.observe(img);
@@ -20,3 +36,6 @@ for (let img of imgs) {
     */
 }
 
+for (let section of sections) {
+    sectObs.observe(section)
+}
