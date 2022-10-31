4d01092ec214d6cab50c1cee9667d697e18027b4
0324bence <bence20050324@gmail.com>
2022-10-31 15:46:52 +0100

    add responsive menu

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let imgs = document.getElementsByClassName("animated-img");
-let sections = document.querySelectorAll("section");
-let uls = document.querySelectorAll("ul[class='animate']");
+const imgs = document.getElementsByClassName("animated-img");
+const sections = document.querySelectorAll("section");
+const uls = document.querySelectorAll("ul[class='animate']");
+const intro = document.getElementById("intro");
+const menuButton = document.getElementById("menu-button");
+const body = document.getElementById("body");
 
 const obs = new IntersectionObserver((entries, obs) => {
-    entries.forEach((entry) => {
+    entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add("animated");
             //console.log(entry);
@@ -11,20 +14,22 @@ const obs = new IntersectionObserver((entries, obs) => {
     });
 });
 
-
-const ratioLimit = 0.522
-const sectObs = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
+const ratioLimit = 0.522;
+const sectObs = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
             //document.querySelector(`[href*='#${entry.target.id}']`).classList.remove("active")
-            console.log(entry.target.id, entry.intersectionRatio)
-        if (entry.isIntersecting && entry.intersectionRatio >= ratioLimit) {
-            document.querySelector(`[href*='#${entry.target.id}']`)?.classList.add("active")
-        } else {
-            document.querySelector(`[href*='#${entry.target.id}']`)?.classList.remove("active")
-            //document.querySelectorAll("#header>a")
-        }
-    })
-}, {threshold: [0.25, ratioLimit, 0.75]})
+            //console.log(entry.target.id, entry.intersectionRatio)
+            if (entry.isIntersecting && entry.intersectionRatio >= ratioLimit) {
+                document.querySelector(`[href*='#${entry.target.id}']`)?.classList.add("active");
+            } else {
+                document.querySelector(`[href*='#${entry.target.id}']`)?.classList.remove("active");
+                //document.querySelectorAll("#header>a")
+            }
+        });
+    },
+    { threshold: [0.25, ratioLimit, 0.75] }
+);
 
 for (let img of imgs) {
     obs.observe(img);
@@ -38,15 +43,15 @@ for (let img of imgs) {
 }
 
 for (let section of sections) {
-    sectObs.observe(section)
+    sectObs.observe(section);
 }
 
 const ulObs = new IntersectionObserver((entries, obs) => {
-    entries.forEach((entry) => {
+    entries.forEach(entry => {
         if (entry.isIntersecting) {
             let ul = entry.target;
-            for (i=0; i < ul.children.length; ++i) {
-                ul.children[i].style.animationDelay = `${i*100+300}ms`;
+            for (i = 0; i < ul.children.length; ++i) {
+                ul.children[i].style.animationDelay = `${i * 100 + 300}ms`;
                 ul.children[i].style.animationPlayState = "running";
             }
         }
@@ -56,3 +61,19 @@ const ulObs = new IntersectionObserver((entries, obs) => {
 for (let ul of uls) {
     ulObs.observe(ul);
 }
+
+let slideMenu;
+
+menuButton.addEventListener("click", async () => {
+    if (!slideMenu) {
+        slideMenu = menuTemplate.content.cloneNode(true);
+        body.insertBefore(menuTemplate.content.cloneNode(true), intro);
+    } else {
+        const elem = document.getElementById("slide-menu");
+        elem.classList.remove("add");
+        elem.classList.add("delete");
+        await new Promise(res => setTimeout(res, 499));
+        elem.remove();
+        slideMenu = undefined;
+    }
+});
